dc11a3c1aa77315ede6b2f1c9b60ad4528d0b383
Agin Elvis <lvsciagin@gmail.com>
2018-05-23 12:53:43 +0300

    Add files via upload

---
+++ writer.c
@@ -0,0 +1,29 @@
+// P15/1726/2016
+// AGIN ELVIS
+// ASSIGNMENT 1 April 22, 2018
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+
+int main()
+{
+	// ftok to generate unique key
+	key_t key = ftok("shmfile",65);
+
+	// shmget returns an identifier in shmid
+	int shmid = shmget(key,1024,0666|IPC_CREAT);
+
+	// shmat to attach to shared memory
+	char *name = (char*) shmat(shmid,(void*)0,0);
+
+	printf ("Write Data : ");
+    scanf ("%s", name);
+
+
+	printf("Data written in memory: %s\n",name);
+	
+	//detach from shared memory 
+	shmdt(name);
+
+	return 0;
+}
